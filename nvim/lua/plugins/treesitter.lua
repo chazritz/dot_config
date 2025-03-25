dcf09ae73c066f7fe34adf15865541fd3404d8fb
Chad Van Klompenburg <chad.vanklompenburg@envoyb2b.com>
2025-03-24 21:56:29 -0400

    initial commit

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+    local tconfig = require("nvim-treesitter.configs")
+    tconfig.setup({
+      ensure_installed = { "c", "lua", "query", "elixir", "heex", "javascript", "html", "python", "go", "zig", "rust", "typescript", "bash" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
