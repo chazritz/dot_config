b25c828274e39507ce726c275562e19aaead39c2
Chad Van Klompenburg <chad.vanklompenburg@envoyb2b.com>
2025-04-10 21:02:56 -0400

    latest

---
+++ .config/nvim/lua/plugins/cursor-smear.lua
@@ -0,0 +1,10 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    --stiffness = 0.5,
+    --trailing_stiffness = 0.5,
+    --distance_stop_animating = 0.5,
+    --trailing_exponent = 3,
+    --gamma = 0.5,
+  },
+}
