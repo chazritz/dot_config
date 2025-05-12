a765cbbde3edafcc0fdbcb9aae9110a8f1cf48f7
Chad Van Klompenburg <chad.vanklompenburg@envoyb2b.com>
2025-05-12 09:19:45 -0400

    lsp/cmp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
+  dpendencies = {
+    'saghen/blink.cmp'
+  },
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = { "LspInfo", "LspInstall", "LspUninstall" },
   config = function()
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
     local lspconfig = require("lspconfig")
-    --lspconfig.zig.setup({
-    --})
+    lspconfig.lua_ls.setup {capabilities = capabilities}
+    lspconfig.zls.setup({ capabilities = capabilities })
   end,
 }
